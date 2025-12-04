b06c597795a45a4d9407ee5dccd12bb27595bf2e
Matt Kane <mkane@cloudflare.com>
2025-12-04 12:11:37 +0000

    Export ProcessInfo type

--- packages/process-ancestry/src/index.ts
+++ packages/process-ancestry/src/index.ts
@@ -2,10 +2,9 @@ import os from "node:os";
 import getAncestryUnix from "./lib/unix.js";
 import getAncestryWindows from "./lib/windows.js";
 import type { ProcessInfo } from "./types.js";
+export type { ProcessInfo };
 
-export function getProcessAncestry(
-  pid = process.pid,
-): Array<ProcessInfo> {
+export function getProcessAncestry(pid = process.pid): Array<ProcessInfo> {
   if (typeof pid !== "number" || !Number.isInteger(pid) || pid <= 0) {
     throw new Error("PID must be a positive integer");
   }
